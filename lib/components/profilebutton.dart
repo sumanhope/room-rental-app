c11a4d8af8553220348e0a361c0a1ca00e30b86d
sumanhope <sumansthahope@gmail.com>
2023-12-13 21:03:05 +0545

    Profile Button Widget

---
+++ lib/components/profilebutton.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:roomrentalapp/themes/colors.dart';
+
+class ProfileButton extends StatelessWidget {
+  const ProfileButton({
+    super.key,
+    required this.size,
+    required this.func,
+    required this.leading,
+    required this.title,
+  });
+
+  final Size size;
+  final VoidCallback func;
+  final IconData leading;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 8,
+        right: 8,
+        top: 8,
+      ),
+      child: SizedBox(
+        width: size.width,
+        height: size.height * 0.07,
+        child: TextButton(
+          onPressed: func,
+          style: TextButton.styleFrom(
+            backgroundColor: AppColor.accentColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7),
+            ),
+          ),
+          child: Row(
+            children: [
+              Icon(
+                leading,
+                size: 27,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: size.width * 0.025,
+              ),
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 0.8,
+                ),
+              ),
+              const Spacer(),
+              const Icon(
+                Icons.keyboard_arrow_right,
+                size: 30,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
