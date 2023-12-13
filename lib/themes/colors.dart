08eafd27208175dc84be9e2ffab52a32efede51f
sumanhope <sumansthahope@gmail.com>
2023-12-13 21:03:20 +0545

    Themes

---
+++ lib/themes/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const Color appbarColor = Color(0xFFFF3D00);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color primaryColor = Color(0xFF000000);
+  static const Color secondaryColor = Color(0xFFC92C04);
+  static const Color accentColor = Color(0xFF8D0000);
+  static const Color accentTextColor = Color(0xF9E0E0E0);
+  static const Color extraColor = Color(0xFFF8D791);
+}
