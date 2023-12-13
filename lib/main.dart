8e1f1546aa40648530a0c1aa6967c496e3cce890
sumanhope <sumansthahope@gmail.com>
2023-12-13 15:27:39 +0545

    all pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:roomrentalapp/pages/favorite.dart';
+import 'package:roomrentalapp/pages/home.dart';
+import 'package:roomrentalapp/pages/profile.dart';
+import 'package:roomrentalapp/pages/rooms.dart';
 
 void main() {
   runApp(const MyApp());
+  GoogleFonts.config.allowRuntimeFetching = false;
 }
 
 class MyApp extends StatelessWidget {
@@ -14,10 +20,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          fontFamily: 'SpaceMono'),
       home: const LandingPage(),
     );
   }
@@ -30,7 +37,12 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-  final List<Widget> pages = [];
+  final List<Widget> pages = [
+    const HomePage(),
+    const RoomPage(),
+    const FavoritePage(),
+    const ProfilePage(),
+  ];
 
   int currentStep = 0;
 
@@ -43,43 +55,38 @@ class _LandingPageState extends State<LandingPage> {
       child: Scaffold(
         body: pages[currentStep],
         bottomNavigationBar: Container(
-          color: Theme.of(context).primaryColor,
+          color: Colors.black,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 11),
             child: GNav(
-              hoverColor: Theme.of(context).hoverColor,
-              backgroundColor: Theme.of(context).colorScheme.background,
-              color: Theme.of(context).unselectedWidgetColor,
-              activeColor: Theme.of(context).focusColor,
-              tabBackgroundColor: Colors.black,
+              hoverColor: Colors.white10,
+              backgroundColor: Colors.black,
+              color: Colors.grey,
+              activeColor: Colors.white,
+              tabBackgroundColor: const Color.fromARGB(255, 54, 53, 53),
               gap: 6,
               padding: const EdgeInsets.all(10),
               iconSize: 25,
-              textStyle: TextStyle(
+              textStyle: const TextStyle(
                 fontSize: 15,
-                color: Theme.of(context).focusColor,
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
-                fontFamily: 'Poppins',
               ),
               tabs: const [
                 GButton(
-                  icon: EvaIcons.home,
-                  text: "Home",
+                  icon: LineAwesome.person_booth_solid,
+                  text: "Welcome",
                 ),
                 GButton(
-                  icon: FontAwesome.searchengin,
+                  icon: FontAwesome.person_shelter,
                   text: "Search",
                 ),
                 GButton(
-                  icon: Icons.speed,
-                  text: "Track",
+                  icon: EvaIcons.heart,
+                  text: "Favorite",
                 ),
                 GButton(
-                  icon: Icons.rice_bowl,
-                  text: "Foods",
-                ),
-                GButton(
-                  icon: Icons.person,
+                  icon: EvaIcons.person,
                   text: "Me",
                 ),
               ],
---
+++ lib/pages/rooms.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RoomPage extends StatefulWidget {
+  const RoomPage({super.key});
+
+  @override
+  State<RoomPage> createState() => _RoomPageState();
+}
+
+class _RoomPageState extends State<RoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Text("Room"),
+      )),
+    );
+  }
+}
