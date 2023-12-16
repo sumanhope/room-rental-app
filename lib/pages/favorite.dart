5d44937e1c7b3e2c540e2c930f517b44e035ecb4
sumanhope <sumansthahope@gmail.com>
2023-12-16 22:05:07 +0545

    SafeArea testing

--- lib/pages/favorite.dart
+++ lib/pages/favorite.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roomrentalapp/themes/colors.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -10,11 +11,32 @@ class FavoritePage extends StatefulWidget {
 class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Text("Favorite"),
-      )),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColor.appbarColor,
+          title: const Text(
+            "Favorite",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              letterSpacing: 1,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text("Favorite"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
