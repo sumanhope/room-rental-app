f944ecd71cbcaf81b6eee16a8b1d9dddcaa58dfb
sumanhope <sumansthahope@gmail.com>
2023-12-16 22:03:59 +0545

    UI of rooms page

--- lib/pages/rooms.dart
+++ lib/pages/rooms.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:roomrentalapp/components/roomcard.dart';
+import 'package:roomrentalapp/themes/colors.dart';
 
 class RoomPage extends StatefulWidget {
   const RoomPage({super.key});
@@ -10,11 +12,48 @@ class RoomPage extends StatefulWidget {
 class _RoomPageState extends State<RoomPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Text("Room"),
-      )),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.appbarColor,
+        title: const Text(
+          "Rooms",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 1,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            RoomCard(
+              roomfloor: "First Floor",
+              uploadDate: "12/16/2023",
+              press: () {},
+              size: size,
+              imagelink: "assets/images/livingroom.jpg",
+            ),
+            RoomCard(
+              roomfloor: "Second Floor",
+              uploadDate: "12/16/2023",
+              press: () {},
+              size: size,
+              imagelink: "assets/images/livingroom.jpg",
+            ),
+            RoomCard(
+              roomfloor: "Third Floor",
+              uploadDate: "12/16/2023",
+              press: () {},
+              size: size,
+              imagelink: "assets/images/livingroom.jpg",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
