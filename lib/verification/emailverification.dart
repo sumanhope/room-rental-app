68bd528f829d6b5892fd98c42e85cbeaf671938d
sumanhope <sumansthahope@gmail.com>
2023-12-24 21:02:37 +0545

    Email Verification UI

---
+++ lib/verification/emailverification.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:keyboard_dismisser/keyboard_dismisser.dart';
+import 'package:roomrentalapp/components/customappbar.dart';
+import 'package:roomrentalapp/components/customoutlinebutton.dart';
+import 'package:roomrentalapp/main.dart';
+import 'package:roomrentalapp/themes/colors.dart';
+import 'package:roomrentalapp/themes/texts.dart';
+
+class EmailVerificationPage extends StatefulWidget {
+  const EmailVerificationPage({super.key});
+
+  @override
+  State<EmailVerificationPage> createState() => _EmailVerificationPageState();
+}
+
+class _EmailVerificationPageState extends State<EmailVerificationPage> {
+  bool isEmailVerified = false;
+  bool canResendEmail = false;
+  //Timer? timer;
+
+  void emailsender() {
+    setState(() {
+      isEmailVerified = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => isEmailVerified
+      ? const LandingPage()
+      : KeyboardDismisser(
+          gestures: const [
+            GestureType.onTap,
+            GestureType.onPanUpdateAnyDirection
+          ],
+          child: Scaffold(
+            appBar: const CustomAppBar(
+              arrow: false,
+              title: "",
+            ),
+            body: Center(
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Opacity(
+                        opacity: 0.5,
+                        child: ClipPath(
+                          clipper: MovieTicketClipper(),
+                          child: Container(
+                            color: AppColor.primaryColor,
+                            height: 110,
+                          ),
+                        ),
+                      ),
+                      ClipPath(
+                        clipper: MovieTicketClipper(),
+                        child: Container(
+                          color: AppColor.primaryColor,
+                          height: 100,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Icon(
+                    Icons.email_outlined,
+                    color: AppColor.accentColor,
+                    size: 150,
+                  ),
+                  const Text(
+                    "Check Your Mail",
+                    style: AppText.orangeXLText,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "We have send a verification link on your email.",
+                      textAlign: TextAlign.center,
+                      style: AppText.blacknormalText,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColor.primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        side: const BorderSide(
+                          width: 2,
+                          color: AppColor.accentColor,
+                        ),
+                      ),
+                      onPressed: emailsender,
+                      child: const Text(
+                        "Resend",
+                        style: AppText.whitenormalbuttonText,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    width: 200,
+                    child: CustomOutlineButton(func: () {}),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                    child: Text(
+                      "Didn't receive email? Check spam filter or try another email! ",
+                      style: AppText.blacksmallText,
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+}
